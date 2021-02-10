b12e7cf7f5f8a8c8d1df243aa50220ca8637149d
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-10 01:39:47 +0000

    [Add] 4-10

--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  devise_for :users
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
