f64f9b501a6d679039b90f0f09f98cf90a03b564
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-11 07:38:24 +0000

    [Add] 4-10

--- Bookers2/config/initializers/application_controller_renderer.rb
+++ Bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '90704572d76f6792f896491811cc547138aa7d659285180b1d9bfa23b72320fd3f96c06c52c67b9327570af5ed9b0147cf715b720d37b0297c80652cffdeafc7'
--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update, :index]
   resources :books, only: [:new, :create, :index, :show, :destroy]
 end
